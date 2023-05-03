16dae0f7055c32f7c0bd3b1426848e0091a6e207
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-05-03 13:02:06 +0900

    cli.tsを作成

---
+++ cli.ts
@@ -0,0 +1,5 @@
+import {
+  removeCache
+} from "./mod.ts";
+
+removeCache(Deno.args[0]);
--- mod.ts
+++ mod.ts
@@ -8,19 +8,26 @@ import {
   type NpmparseReturn,
 } from "./utils.ts";
 
-
-const removePath = Deno.args[0];
-const denodir = (await getDenoDir()).replace("DENO_DIR location","").split(" ").slice(1).join("");
-const url = new URL(removePath);
-let removeFilePath;
-if(["https:","http:"].includes(url.protocol)){
-  removeFilePath = path.join(denodir,"deps",url.protocol.slice(0,-1),url.host);
-}else if(url.protocol=== "npm:"){
-  const { reponame,  version } = npmparse(url.pathname);
-  removeFilePath = path.join(denodir,"npm/registry.npmjs.org",reponame,version);
-}else if(url.protocol === "all:"){
-  removeFilePath = denodir;
+async function removeCache(removePath: string): Promise<void>{
+  const denodir = (await getDenoDir()).replace("DENO_DIR location","").split(" ").slice(1).join("");
+  const url = new URL(removePath);
+  let removeFilePath: string;
+  if(["https:","http:"].includes(url.protocol)){
+    removeFilePath = path.join(denodir,"deps",url.protocol.slice(0,-1),url.host);
+  }else if(url.protocol=== "npm:"){
+    const { reponame,  version } = npmparse(url.pathname);
+    removeFilePath = path.join(denodir,"npm/registry.npmjs.org",reponame,version);
+  }else if(url.protocol === "all:"){
+    removeFilePath = denodir;
+  }
+  if(await isDir(removeFilePath)){
+    await Deno.remove(removeFilePath, { recursive: true });
+  }
 }
-if(await isDir(removeFilePath)){
-  await Deno.remove(removeFilePath, { recursive: true });
+
+export {
+  isdir,
+  npmparse,
+  type NpmparseReturn,
+  removeCache,
 }
