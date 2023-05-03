c68b9d328b4ddf493275eb3224c1318050bc7680
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-05-03 12:43:07 +0900

    add getDenoDir import

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import * as path from "https://deno.land/std@0.185.0/path/mod.ts";
+import { getDenoDir } from "https://gist.githubusercontent.com/kawarimidoll/92179f60dfc67de3b0a52c5eb25ad333/raw/b193e088e47ee033a7d685ca6e9f45793bd26844/get_deno_dir.ts"
 
 async function isDir(path: string): Promise<boolean> {
   try {
